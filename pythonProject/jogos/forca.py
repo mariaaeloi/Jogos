42d277326d1ca18b138f7255c56c17dc1adf524e
Maria Angela Eloi <mariaangelaeloidejesus@hotmail.com>
2022-10-02 14:46:08 -0300

    Add files via upload

---
+++ pythonProject/jogos/forca.py
@@ -0,0 +1,42 @@
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de forca!******")
+    print("*********************************")
+
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                     letras_acertadas[index] = letra
+                index = index + 1
+        else:
+            erros = erros + 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+        print("Jogando...")
+
+
+    if (acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+    print("Fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
